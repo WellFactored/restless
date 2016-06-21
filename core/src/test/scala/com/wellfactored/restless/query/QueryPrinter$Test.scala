cf60fa83b530f86e0dc831f00972ea3d060240f8
Doug Clinton <doug@wellfactored.com>
2016-06-21 19:04:47 +0100

    Updated QueryPrinterTest to give 100% coverage

--- core/src/test/scala/com/wellfactored/restless/query/QueryPrinter$Test.scala
+++ core/src/test/scala/com/wellfactored/restless/query/QueryPrinter$Test.scala
@@ -2,72 +2,79 @@ package com.wellfactored.restless.query
 
 import atto.Atto._
 import com.wellfactored.restless.query.QueryParser._
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FlatSpec, Matchers, WordSpecLike}
 import QueryPrinter.print
+import com.wellfactored.restless.query.QueryAST.All
 
-class QueryPrinter$Test extends FlatSpec with Matchers {
+class QueryPrinter$Test extends WordSpecLike with Matchers {
 
-  "query printer" should "normalise a number to 1 dp" in {
-    val q = "a = 1"
-    val expected = "a = 1.0"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(expected)
-  }
+  "query printer" should {
+    "print All as 'all'" in {
+      print(All) shouldBe "all"
+    }
 
-  it should "print a path equality" in {
-    val q = "a = b"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a path inequality" in {
-    val q = "a != b"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a path gt" in {
-    val q = "a > b"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a path lt" in {
-    val q = "a < b"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a path ge" in {
-    val q = "a >= b"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a path le" in {
-    val q = "a <= b"
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
+    "normalise a number to 1 dp" in {
+      val q = "a = 1"
+      val expected = "a = 1.0"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(expected)
+    }
 
-  it should "print a string equality" in {
-    val q = """a = "x""""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a string inequality" in {
-    val q = """a != "x""""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a string 'starts with'" in {
-    val q = """a starts with "x""""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a string 'ends with'" in {
-    val q = """a ends with "x""""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
-  it should "print a string 'contains'" in {
-    val q = """a contains "x""""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(q)
-  }
+    "print a path equality" in {
+      val q = "a = b"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a path inequality" in {
+      val q = "a != b"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a path gt" in {
+      val q = "a > b"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a path lt" in {
+      val q = "a < b"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a path ge" in {
+      val q = "a >= b"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a path le" in {
+      val q = "a <= b"
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
 
-  it should "normalise an 'and'" in {
-    val q = """a = 1 and b = "x""""
-    val expected = """(a = 1.0) and (b = "x")"""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(expected)
-  }
-  it should "normalise an 'or'" in {
-    val q = """a = 1 or b = "x""""
-    val expected = """(a = 1.0) or (b = "x")"""
-    query.parseOnly(q).done.option.map(print) shouldBe Some(expected)
-  }
+    "print a string equality" in {
+      val q = """a = "x""""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a string inequality" in {
+      val q = """a != "x""""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a string 'starts with'" in {
+      val q = """a starts with "x""""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a string 'ends with'" in {
+      val q = """a ends with "x""""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
+    "print a string 'contains'" in {
+      val q = """a contains "x""""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(q)
+    }
 
+    "normalise an 'and'" in {
+      val q = """a = 1 and b = "x""""
+      val expected = """(a = 1.0) and (b = "x")"""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(expected)
+    }
+    "normalise an 'or'" in {
+      val q = """a = 1 or b = "x""""
+      val expected = """(a = 1.0) or (b = "x")"""
+      query.parseOnly(q).done.option.map(print) shouldBe Some(expected)
+    }
+  }
 }
+
